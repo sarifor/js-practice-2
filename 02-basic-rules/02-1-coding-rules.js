d968a96883e332773ee3881863cf6acb47113edd
sarifor <iread1q84@gmail.com>
2023-06-30 09:29:57 +0900

    Practice semicolon, comment, and indent

---
+++ 02-basic-rules/02-1-coding-rules.js
@@ -0,0 +1,11 @@
+// Semicolon
+console.log('Hello, world!'); console.log('Hi'); console.log('Ohayo');
+
+// Comment
+/* console.log('bye'); */
+
+// Indent
+let condition = 1
+if (condition) {
+    console.log('world');
+}
