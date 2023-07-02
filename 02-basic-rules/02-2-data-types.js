06b50d2754a497a5cd86959ce16f65856dd802c2
sarifor <iread1q84@gmail.com>
2023-07-02 21:59:43 +0900

    Practice logical operators

--- 02-basic-rules/02-2-data-types.js
+++ 02-basic-rules/02-2-data-types.js
@@ -79,3 +79,11 @@ console.log(1 != '1'); // false
 console.log('1' === 1); // false
 console.log(1 === true); // false
 console.log(1 !== '1'); // true
+
+// Logical operators
+console.log(Boolean(false)); // false
+console.log(Boolean('')); // false
+console.log(Boolean(0)); // false
+console.log(Boolean(NaN)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean(null)); // false
