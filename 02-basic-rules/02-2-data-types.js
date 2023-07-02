de20e834b676cb259854156bf34978514827d2a0
sarifor <iread1q84@gmail.com>
2023-07-02 20:02:44 +0900

    Practice backtick

--- 02-basic-rules/02-2-data-types.js
+++ 02-basic-rules/02-2-data-types.js
@@ -8,3 +8,13 @@ console.log(result);
 // console.log('string has single quote(')'); // SyntaxError: Invalid or unexpected token
 console.log("string has single quote(')"); // No Error
 console.log('string has single quote(\')'); // No Error
+
+// multiple lines
+console.log("Good\n\nNight!");
+
+// backtick
+console.log(`string`);
+console.log(``);
+console.log(`backtick(\`) is in string`);
+console.log(`Good
+Night!`);
