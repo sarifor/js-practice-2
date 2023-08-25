26161cc35b6d94cbeec9011d88831ca9f366c29a
sarifor <iread1q84@gmail.com>
2023-08-25 17:58:30 +0900

    Practice another use of arr.reduce

--- 08-Date/arr.reduce.js
+++ 08-Date/arr.reduce.js
@@ -1,4 +1,13 @@
+// Basic use
 const array1 = [1, 2, 3, 4, 5];
 const initialValue = 0; // If there is no initial, then reduce method takes the first element of the array as the initial value
 const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator + currentValue);
 console.log(sumWithInitial); // 15
+
+// Another use
+const result = [1, 2, 3, 4, 5].reduce((a, c) => {
+  // console.log(a);
+  a[c] = c * 10;
+  return a;
+}, {}); // Initial value '{}' is assigned
+console.log(result); // { '1': 10, '2': 20, '3': 30, '4': 40, '5': 50 }
